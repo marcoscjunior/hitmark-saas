7a66d84cbb5a15e2463363d02fe6cc8d908b9a91
marcoscjunior <100360607+marcoscjunior@users.noreply.github.com>
2026-04-29 12:16:20 -0300

    Add TypeScript types to RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
-// app/layout.tsx
 import './globals.css';
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
